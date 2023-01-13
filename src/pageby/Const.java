844a0230c3bbc4a0300482a595a853afe2e8d96c
meghamapala@gmail.com <meghamapala@gmail.com>
2023-01-13 02:44:25 -0500

    first  commit

---
+++ src/pageby/Const.java
@@ -0,0 +1,28 @@
+package pageby;
+
+public class Const {
+	
+	String d;
+	String b;
+	int l;
+	
+	Const(){
+		
+	}
+	
+    Const(String d){
+    	this.d=d;
+		
+	}
+    
+    int add(int l, int c) {
+    	
+    	int T=l+c;
+    	
+    	return T;
+    	
+    }
+	
+	
+
+}
